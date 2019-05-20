9db1883797f4ed8cc380a5d5d27f9bd8a08fb5d4
SAKIL-PC\Tajul Islam <shakilreyan9397@gmail.com>
2019-05-20 23:03:35 +0600

    test

--- wp-content/themes/mntn/functions.php
+++ wp-content/themes/mntn/functions.php
@@ -460,3 +460,34 @@ function bbloomer_add_new_order_admin_list_column_content( $column ) {
       
     }
 }	
+
+/** 
+ * Register new status
+ * Tutorial: http://www.sellwithwp.com/woocommerce-custom-order-status-2/
+**/
+function register_awaiting_shipment_order_status() {
+    register_post_status( 'wc-awaiting-shipment', array(
+        'label'                     => 'Awaiting shipment',
+        'public'                    => true,
+        'exclude_from_search'       => false,
+        'show_in_admin_all_list'    => true,
+        'show_in_admin_status_list' => true,
+        'label_count'               => _n_noop( '出荷待ち <span class="count">(%s)</span>', '出荷待ち <span class="count">(%s)</span>' )
+    ) );
+}
+add_action( 'init', 'register_awaiting_shipment_order_status' );
+// Add to list of WC Order statuses
+function add_awaiting_shipment_to_order_statuses( $order_statuses ) {
+    $new_order_statuses = array();
+    // add new order status after processing
+    foreach ( $order_statuses as $key => $status ) {
+        $new_order_statuses[ $key ] = $status;
+        if ( 'wc-processing' === $key ) {
+            $new_order_statuses['wc-awaiting-shipment'] = '出荷待ち';
+        }
+    }
+    return $new_order_statuses;
+}
+
+add_filter( 'wc_order_statuses', 'add_awaiting_shipment_to_order_statuses' );
+
