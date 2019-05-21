103d4f27d643a7946fee7e8e8f250fe6456ee98a
SAKIL-PC\Tajul Islam <shakilreyan9397@gmail.com>
2019-05-21 21:05:36 +0600

    test

--- tshirtecommerce/assets/js/design.js
+++ tshirtecommerce/assets/js/design.js
@@ -2663,7 +2663,7 @@ var design={
 			$jd('.ui-lock').attr('checked', false);
 			var txt = {};
 			
-			txt.text = 'テキストを\n入力して\nください';
+			txt.text = 'テキストを入力してください';
 			txt.color = '#FF0000';
 			txt.fontSize = '24px';
 			txt.fontFamily = 'arial';
