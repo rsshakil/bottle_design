dc21c2c635e178e224e06ebf2740c132c686c3f9
SAKIL-PC\Tajul Islam <shakilreyan9397@gmail.com>
2019-05-24 11:59:11 +0600

    test

--- wp-content/plugins/woocommerce/templates/myaccount/dashboard.php
+++ wp-content/plugins/woocommerce/templates/myaccount/dashboard.php
@@ -25,20 +25,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 <p><?php
 	/* translators: 1: user display name 2: logout url */
 	printf(
-		__( 'Hello %1$s (not %1$s? <a href="%2$s">Log out</a>)', 'woocommerce' ),
-		'<strong>' . esc_html( $current_user->display_name ) . '</strong>',
+		__( 'ようこそ %1$s (様ではない場合はログアウトしてください %1$s? <a href="%2$s">Log out</a>)', 'woocommerce' ),
+		'<strong>' . esc_html( $current_user->display_name ) . '様</strong>',
 		esc_url( wc_logout_url( wc_get_page_permalink( 'myaccount' ) ) )
 	);
 ?></p>
 
-<p><?php
+<!--<p><?php
 	printf(
 		__( 'From your account dashboard you can view your <a href="%1$s">recent orders</a>, manage your <a href="%2$s">shipping and billing addresses</a>, and <a href="%3$s">edit your password and account details</a>.', 'woocommerce' ),
 		esc_url( wc_get_endpoint_url( 'orders' ) ),
 		esc_url( wc_get_endpoint_url( 'edit-address' ) ),
 		esc_url( wc_get_endpoint_url( 'edit-account' ) )
 	);
-?></p>
+?></p>-->
 
 <?php
 	/**
